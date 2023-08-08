37f48b0301f724e68a134bfbcef7e13811b56e74
Lukas Jonca <lukas.jonca.lj@gmail.com>
2023-08-08 11:55:36 +0300

    Implemented page CRUD

--- backend/src/main/java/com/tietoevry/backend/database/repository/PageRepository.java
+++ backend/src/main/java/com/tietoevry/backend/database/repository/PageRepository.java
@@ -1,9 +1,13 @@
 package com.tietoevry.backend.database.repository;
 
+import java.util.List;
+
 import com.tietoevry.backend.database.entity.PageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PageRepository extends JpaRepository<PageEntity, Long> {
+    List<PageEntity> findByNewsletter_NewsletterId(Long newsletterId);
+
 }
