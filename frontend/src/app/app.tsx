5c5791ddb664c031a02164361708dff8d502fe1f
Julius Kaulinis <kaulinis.julius@gmail.com>
2023-08-10 13:49:15 +0300

    Created footer

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { Header } from '@app/components/header';
+import { Footer } from '@app/components/footer/footer.tsx';
 
 const App: React.FC = () => (
   <>
     <Header />
     <Outlet />
+    <Footer />
   </>
 );
 
---
+++ frontend/src/app/components/footer/footer.styled.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+import { spacing2 } from '@app/styles/spacing.ts';
+import { colorPrimary, colorWhite } from '@app/styles/colors.ts';
+import logoWhite from '@app/components/footer/logo/logoWhite.png';
+
+export const StyledFooter = styled.div`
+  height: 48px;
+  background: ${colorPrimary};
+  color: ${colorWhite};
+  padding: ${spacing2};
+  background-image: url('${logoWhite}');
+  background-repeat: repeat;
+  background-size: contain;
+`;
