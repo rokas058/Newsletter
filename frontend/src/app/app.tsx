4f6d638e36f05ef6725aedbbc1f14eeca59eb4cb
Justinas <justinasve@gmail.com>
2023-08-16 13:23:58 -0700

    updated header on login page, footer bug fixed

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 import { Header } from '@app/components/header';
 import { Footer } from '@app/components/footer/footer.tsx';
+import { NavigationService } from '@app/services/navigation-service.ts';
+import { FooterLogin } from '@app/components/footer/footer-login.tsx';
 
-const App: React.FC = () => (
-  <>
-    <Header />
-    <Outlet />
-    <Footer />
-  </>
-);
+const App: React.FC = () => {
+  const location = useLocation();
+
+  const loginPage = location.pathname === NavigationService.LOGIN_PATH;
+
+  return (
+    <>
+      <Header />
+      <Outlet />
+      {loginPage ? <FooterLogin /> : <Footer />}
+    </>
+  );
+};
 
 export { App };
--- frontend/src/app/components/footer/footer-login.styled.ts
+++ frontend/src/app/components/footer/footer-login.styled.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
-import { spacing12, spacing2 } from '@app/styles/spacing.ts';
 import { colorPrimary, colorWhite } from '@app/styles/colors.ts';
+import { spacing12, spacing2 } from '@app/styles/spacing.ts';
 import logoWhite from '@app/components/footer/logo/logoWhite.png';
 
-export const StyledFooter = styled.div`
+export const StyledFooterLogin = styled.div`
   height: 48px;
   background: ${colorPrimary};
   color: ${colorWhite};
@@ -13,4 +13,8 @@ export const StyledFooter = styled.div`
   background-repeat: repeat;
   background-size: contain;
   margin-top: ${spacing12};
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
 `;
