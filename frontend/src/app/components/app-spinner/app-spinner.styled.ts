1def618cfb96ee8dfc6b716af1add52e5c6e37a3
Justinas <justinasve@gmail.com>
2023-08-18 00:29:35 -0700

    PMSI2: fixed loading state

--- frontend/src/app/components/app-spinner/app-spinner.styled.ts
+++ frontend/src/app/components/app-spinner/app-spinner.styled.ts
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 export const StyledSpinner = styled(Spin)`
   width: 100%;
   height: 100vh;
-  position: absolute;
   top: 0;
   display: flex;
   justify-content: center;
