f4d25d9430c7a5b9eab21d5d47e969ddd83bdda8
Julius Kaulinis <kaulinis.julius@gmail.com>
2023-08-22 19:23:01 +0300

    Added content to Ukraine page

--- frontend/src/app/page/ukraina/ukraina.styled.ts
+++ frontend/src/app/page/ukraina/ukraina.styled.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import { colorWhite, colorYellow } from '@app/styles/colors.ts';
+import { colorBlack, colorWhite, colorYellow } from '@app/styles/colors.ts';
 
 export const StyledPageContainer = styled.div`
   background: linear-gradient(to bottom, #1e90ff 50%, ${colorYellow} 50%);
@@ -17,50 +17,79 @@ export const StyledHeader = styled.h1`
 `;
 
 export const StyledContentContainer = styled.div`
-  width: 90%;
-  margin: 0 0 30px;
+  width: 60%;
+  margin: 0 0 80px;
   background-color: ${colorWhite};
-  padding: 10px 25px;
   font-size: 25px;
-  border: solid 2px ${colorWhite};
+  border: solid 0 ${colorWhite};
   border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  flex-wrap: wrap;
 `;
 
-export const StyledMainParagraph = styled.p``;
+export const StyledMainParagraph = styled.p`
+  text-align: center;
+  font-weight: bold;
+  font-size: 40px;
+  padding: 20px 50px;
+`;
 
-export const StyledInsideContentContainer = styled.div``;
+export const StyledUkraineImage = styled.img`
+  width: 100%;
+  border: solid 0;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+`;
 
 export const StyledSpan = styled.span`
   font-weight: bold;
+  color: #1e90ff;
 `;
 
-export const StyledUnorderedList = styled.ul`
+export const StyledParagraph = styled.p`
+  text-align: justify;
+  padding: 0 30px;
+`;
+
+export const StyledLinkDiv = styled.div`
+  width: 48%;
+  gap: 10px;
   display: flex;
   flex-direction: column;
-  flex-wrap: wrap;
-  text-align: justify;
-  min-height: 600px;
-  gap: 20px;
+  margin-bottom: 30px;
 `;
 
-export const StyledListItem = styled.li`
-  max-width: 45%;
+export const StyledMiniDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: 30px;
 `;
-
 export const StyledAnchorElement = styled.a`
   font-weight: bold;
-  font-size: 35px;
+  font-size: 25px;
   text-decoration: none;
+  color: ${colorBlack};
 
   &:visited {
     text-decoration: none;
+    color: ${colorBlack};
   }
 
   &:link {
     text-decoration: none;
+    color: ${colorBlack};
   }
 
   &:active {
     text-decoration: none;
+    color: ${colorBlack};
+  }
+
+  &:hover {
+    color: ${colorYellow};
   }
 `;
--- frontend/src/app/page/ukraina/ukraina.tsx
+++ frontend/src/app/page/ukraina/ukraina.tsx
@@ -2,106 +2,117 @@ import {
   StyledAnchorElement,
   StyledContentContainer,
   StyledHeader,
-  StyledInsideContentContainer,
-  StyledListItem,
+  StyledLinkDiv,
   StyledMainParagraph,
+  StyledMiniDiv,
   StyledPageContainer,
+  StyledParagraph,
   StyledSpan,
-  StyledUnorderedList,
+  StyledUkraineImage,
 } from '@app/page/ukraina/ukraina.styled.ts';
+import ukraineImage from '@app/assets/images/standWithUkraine.jpg';
 
 export const UkrainaPage = () => (
   <StyledPageContainer>
     <StyledHeader>STAND WITH UKRAINE</StyledHeader>
     <StyledContentContainer>
+      <StyledUkraineImage src={ukraineImage} />
       <StyledMainParagraph>
         Nors praėjo jau daug laiko, nepamirškime, kad Ukraina vis dar kovoja už
         mus visus, ir mes turime jiems padėti 💙
       </StyledMainParagraph>
-      <StyledInsideContentContainer>
-        <StyledSpan>Tietoevry naujienos:</StyledSpan>
-        <StyledUnorderedList>
-          <StyledListItem>
-            Maždaug 90% mūsų Ukrainos komandos narių yra vietovėse, kuriose
-            numatomas mažesnis rizikos lygis, tačiau turime dešimtis kolegų,
-            kurie nuolat tarnauja ginkluotosiose pajėgose. Mes ir toliau kasdien
-            stebime savo komandos narių saugumą.
-          </StyledListItem>
-          <StyledListItem>
-            Jau daugiau, kaip metus Ukraina kovoja už savo ir visų laisvojo
-            pasaulio žmonių laisvę. Vakarų pasaulis ir toliau laikosi vieningos
-            pozicijos su Ukraina, įskaitant mus, esančius Tietoevryje. Vieną
-            dieną ir tai baigsis - tikėkimės, kad ta diena ateis greitai. O mes
-            ir toliau laikysimės kartu su Ukraina, kiek tik reikės.
-          </StyledListItem>
-          <StyledListItem>
-            Tietoevry inicijuoja darbuotojų lėšų rinkimo kampaniją per
-            Norvegijos lėšų rinkimo platformą "Spleis". Kampanijos metu renkami
-            pinigai bus aukojami tiesiogiai mūsų kolegų, žuvusių ar sužeistų per
-            karą, šeimoms.
-          </StyledListItem>
+
+      <StyledSpan style={{ fontSize: '30px' }}>Tietoevry naujienos:</StyledSpan>
+      <StyledParagraph>
+        Maždaug 90% mūsų Ukrainos komandos narių yra vietovėse, kuriose
+        numatomas mažesnis rizikos lygis, tačiau turime dešimtis kolegų, kurie
+        nuolat tarnauja ginkluotosiose pajėgose. Mes ir toliau kasdien stebime
+        savo komandos narių saugumą.
+      </StyledParagraph>
+      <StyledParagraph>
+        Jau daugiau, kaip metus Ukraina kovoja už savo ir visų laisvojo pasaulio
+        žmonių laisvę. Vakarų pasaulis ir toliau laikosi vieningos pozicijos su
+        Ukraina, įskaitant mus, esančius Tietoevryje. Vieną dieną ir tai baigsis
+        - tikėkimės, kad ta diena ateis greitai. O mes ir toliau laikysimės
+        kartu su Ukraina, kiek tik reikės.
+      </StyledParagraph>
+      <StyledParagraph>
+        Tietoevry inicijuoja darbuotojų lėšų rinkimo kampaniją per Norvegijos
+        lėšų rinkimo platformą "Spleis". Kampanijos metu renkami pinigai bus
+        aukojami tiesiogiai mūsų kolegų, žuvusių ar sužeistų per karą, šeimoms.
+      </StyledParagraph>
+      <StyledLinkDiv>
+        <StyledMiniDiv>
+          <StyledSpan>Paaukok</StyledSpan>
           <StyledAnchorElement href="https://www.spleis.no/PROJECT/218485">
-            ➡️ Aukojimo nuoroda
+            Aukojimo nuoroda 💙
           </StyledAnchorElement>
-          <StyledListItem>
-            Visa informacija atvykstantiems ukrainiečiams ➡️
-          </StyledListItem>
-          <StyledAnchorElement href="https://helpua.lt/">
-            HelpUA
-          </StyledAnchorElement>
-          <StyledListItem>Paaukok ➡️</StyledListItem>
           <StyledAnchorElement href="https://www.blue-yellow.lt/">
-            BLUE-YELLOW
+            BLUE-YELLOW 💛
           </StyledAnchorElement>
+        </StyledMiniDiv>
+        <StyledMiniDiv>
+          <StyledSpan>Visa informacija atvykstantiems ukrainiečiams</StyledSpan>
+          <StyledAnchorElement href="https://helpua.lt/">
+            HelpUA 💙
+          </StyledAnchorElement>
+        </StyledMiniDiv>
+        <StyledMiniDiv>
+          <StyledSpan>LT847300010171415974</StyledSpan>
+          <StyledSpan>VŠĮ VIENAS K PARAMOS FONDAS</StyledSpan>
+          <StyledSpan>Įmonės kodas: 306036141</StyledSpan>
+          <StyledSpan>Paskirtis: Parama</StyledSpan>
+          <StyledSpan>Adresas: Vilnius, Peteliškių g. 9-10</StyledSpan>
+          <StyledSpan>Direktorius: Oleg Šurajev</StyledSpan>
           <StyledAnchorElement href="https://www.aukok.lt/proje.../Musu-sirdys-ir-rankos-Ukrainai">
-            Mūsų rankos ir širdys Ukrainai
+            1K Paramos Fondas 💛
           </StyledAnchorElement>
-          <span>(Trumpasis numeris: 1485)</span>
-          <StyledAnchorElement href="https://www.aukok.lt/proje.../Musu-sirdys-ir-rankos-Ukrainai">
-            1K Paramos Fondas
-          </StyledAnchorElement>
-          <span>LT847300010171415974</span>
-          <span>VŠĮ VIENAS K PARAMOS FONDAS</span>
-          <span>Įmonės kodas: 306036141</span>
-          <span>Paskirtis: Parama</span>
-          <span>Adresas: Vilnius, Peteliškių g. 9-10</span>
-          <span>Direktorius: Oleg Šurajev</span>
-          <StyledListItem>Savanoriauk ➡️</StyledListItem>
+        </StyledMiniDiv>
+      </StyledLinkDiv>
+      <StyledLinkDiv>
+        <StyledMiniDiv>
+          <StyledSpan>Savanoriauk</StyledSpan>
           <StyledAnchorElement href="https://www.redcross.lt/savanoriaukite">
-            Raudonasis kryžius
+            Raudonasis kryžius 💙
           </StyledAnchorElement>
           <StyledAnchorElement href="https://www.sauliusajunga.lt/">
-            Šaulių sąjunga
+            Šaulių sąjunga 💛
           </StyledAnchorElement>
           <StyledAnchorElement href="http://www.maistobankas.lt/reg">
-            Maisto bankas
+            Maisto bankas 💙
           </StyledAnchorElement>
           <StyledAnchorElement href="https://forms.gle/EkAD8iSJt4aYYr3x8">
-            Lietuvos Caritas
+            Lietuvos Caritas 💛
           </StyledAnchorElement>
           <StyledAnchorElement href="https://forms.office.com/r/E3gGXPpkvB">
-            Maltos ordino Pagalbos Tarnyba
+            Maltos ordino Pagalbos Tarnyba 💙
           </StyledAnchorElement>
-          <StyledListItem>Apgyvendinimas ir pavežėjimas️</StyledListItem>
+        </StyledMiniDiv>
+        <StyledMiniDiv>
+          <StyledSpan>Apgyvendinimas ir pavežėjimas️</StyledSpan>
           <StyledAnchorElement href="https://stipruskartu.lt/">
-            Maltos ordino Pagalbos Tarnyba
+            Maltos ordino Pagalbos Tarnyba 💛
           </StyledAnchorElement>
-          <StyledListItem>Gyvūnų globa</StyledListItem>
+        </StyledMiniDiv>
+        <StyledMiniDiv>
+          <StyledSpan>Gyvūnų globa</StyledSpan>
           <StyledAnchorElement href="https://bit.ly/PrieglobstisGyvunams">
-            CGI
+            CGI 💙
           </StyledAnchorElement>
-          <StyledListItem>Sekite korporacijos naujienas:</StyledListItem>
+        </StyledMiniDiv>
+        <StyledMiniDiv>
+          <StyledSpan>Sekite korporacijos naujienas</StyledSpan>
           <StyledAnchorElement href="https://tietoevry.sharepoint.com/sites/ourcompany/SitePages/Ukraine.aspx">
-            Naujienos apie situaciją Ukrainoje
+            Naujienos apie situaciją Ukrainoje 💛
           </StyledAnchorElement>
           <StyledAnchorElement href="https://tietoevry.workplace.com/groups/265896197727289s">
-            Workplace Tietoevry news grupė
+            Workplace Tietoevry news grupė 💙
           </StyledAnchorElement>
           <StyledAnchorElement href="https://tietoevry.sharepoint.com/sites/myemployment/SitePages/War-in-Ukraine-u.aspx">
-            Tietoevry Lietuva Ukrainai skirtas puslapis
+            Tietoevry Lietuva Ukrainai skirtas puslapis 💙
           </StyledAnchorElement>
-        </StyledUnorderedList>
-      </StyledInsideContentContainer>
+        </StyledMiniDiv>
+      </StyledLinkDiv>
     </StyledContentContainer>
   </StyledPageContainer>
 );
